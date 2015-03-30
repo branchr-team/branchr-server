11b2c80a9a43b62f68dd29316b83e4a3c60e9472
Josh Glendenning <joshglendenning@gmail.com>
2015-03-30 16:41:22 -0500

    ~

--- src/app.js
+++ src/app.js
@@ -4,10 +4,27 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 export function run() {
+	// Create new express app
 	var app = new express();
+
+	//// Load middleware
+
+	// CORS
+	app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+	});
+
+	// Body parsing
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
+
+	// Main router
 	app.use(routes.router())
+
+	//// Start app
+
 	return app.listen(config.PORT, function() {
 		console.log(`Listening on port ${config.PORT}`);
 	});
