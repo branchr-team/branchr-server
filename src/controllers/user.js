f5466232cb4953e9433f155be9e8fb29df64b2d1
Josh Glendenning <joshglendenning@gmail.com>
2015-03-30 16:35:51 -0500

    ~

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,7 +4,10 @@ import * as UserService from 'services/user';
 export default new Controller((router) => {
 
 	router.post('/:name', (req, res) => {
-		UserService.register(req.body)
+		if (req.params.name === 'foo') res.status(400).send({msg: 'name parameter cannot be foo'})
+		UserService.register({
+			name: req.params.name
+		})
 			.then(user => {
 				res.status(200).send(user);
 			})
@@ -14,12 +17,13 @@ export default new Controller((router) => {
 	});
 
 	router.get('/:name', (req, res) => {
+		if (req.params.name === 'foo') res.status(400).send({msg: 'name parameter cannot be foo'})
 		UserService.getByName(req.params.name)
 			.then(user => {
 				res.status(200).send(user);
 			})
 			.catch(err => {
-				res.status(500).send(err);
+				res.status(err.status || 500).send(err);
 			});
 	});
 
--- src/services/user.js
+++ src/services/user.js
@@ -3,9 +3,16 @@ import {db} from 'db';
 var users = db.collection('users');
 
 export function register(user) {
+	console.log(`Registering user with name ${user.name}`);
 	return users.insert(user);
 }
 
 export function getByName(name) {
-	return users.findOne({'name': name});
+	console.log(`Finding user by name ${name}`);
+	return users.findOne({'name': name}).then((result) => {
+		return new Promise((resolve, reject) => {
+			if (result == null) reject({status: 404, msg: `Could not find user ${name}!`});
+			else resolve(result);
+		});
+	});
 }
