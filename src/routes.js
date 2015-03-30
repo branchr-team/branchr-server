df2151aa424aded1a5315584ade90f4f44f7181c
Josh Glendenning <joshglendenning@gmail.com>
2015-03-30 16:16:00 -0500

    ~

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import UserController from 'controllers/user';
 export function router() {
 	var router = require('express').Router();
 
+	router.get('/', (req, res) => {
+		res.send({msg: 'Hullo'});
+	});
 	router.use('/user', UserController.router);
 
 	return router;
