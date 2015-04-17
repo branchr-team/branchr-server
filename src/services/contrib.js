d33cdd1950089c7cfa18fe77e2f2631f122c23cf
Josh Glendenning <joshglendenning@gmail.com>
2015-04-17 13:10:28 -0500

    ~

--- src/services/contrib.js
+++ src/services/contrib.js
@@ -5,7 +5,16 @@ var ObjectId = pmongo.ObjectId;
 var contribs = db.collection('contribs');
 
 export function getById(id) {
-	return contribs.findOne({_id: ObjectId(id)}).then(result => {
+	return contribs.findOne({_id: ObjectId(id)})
+	.then(result => new Promise((resolve, reject) => {
+		if (!result) resolve(null);
+		db.collection('engine').findOne({id: result.engine})
+			.then(engineResult => {
+				result.js = engineResult.js;
+				resolve(result);
+			});
+	}))
+	.then(result => {
 		return new Promise((resolve, reject) => {
 			if (result == null) reject({status: 404, msg: ''});
 			else resolve(result);
