0a8db3ac93a21ee106afeb0b9aad12eed5da6218
Josh Glendenning <joshglendenning@gmail.com>
2015-05-06 16:35:40 -0500

    ~

--- src/controllers/engine.js
+++ src/controllers/engine.js
@@ -6,7 +6,7 @@ export default new Controller(router => {
 	router.get('/:engineId', (req, res) => {
 		Engine.findById(req.params.engineId, function(err, result) {
 			if (err) 
-				res.status(err.status || 500).send(err);
+				res.status(500).send(err);
 			else if (!result)
 				res.status(404).send();
 			else
@@ -17,7 +17,7 @@ export default new Controller(router => {
 	router.post('', (req, res) => {
 		Engine.create(req.body, function(err, result) {
 			if (err) 
-				res.status(err.status || 500).send(err);
+				res.status(500).send(err);
 			else
 				res.status(200).send(result);
 		});
