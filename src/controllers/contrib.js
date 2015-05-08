4c52bcf78497e0f6ed85fe36fadf8c8575ac0448
Josh Glendenning <joshglendenning@gmail.com>
2015-05-08 17:40:22 -0500

    ~

--- src/controllers/contrib.js
+++ src/controllers/contrib.js
@@ -1,25 +1,33 @@
 import {Controller} from 'lib/controller';
 import {Contrib} from 'models/contrib';
+import {User} from 'models/user';
 import {auth} from 'controllers/auth';
 
 export default new Controller(router => {
 
-	router.get('/:contribId', (req, res) => {
+    router.get('/:contribId', (req, res) => {
         let dbQuery = Contrib.findById(req.params.contribId);
         dbQuery.populate('creator', 'username');
         dbQuery.populate('feed');
         dbQuery.exec(function(err, result) {
             if (err)
-                res.status(err.status || 500).send(err);
+                res.status(500).send(err);
             else if (!result)
                 res.status(404).send();
             else
-                res.status(200).send(result);
+                User.populate(result, {path: 'feed.owners', select: 'username'}, function(err2, result2) {
+                    if (err2)
+                        res.status(500).send(err2);
+                    else if (!result2)
+                        res.status(404).send();
+                    else
+                        res.status(200).send(result2);
+                });
         });
-	});
+    });
 
-	// DELETE /contrib/:contribId
-	router.delete('/:contribId', auth, (req, res) => {
+    // DELETE /contrib/:contribId
+    router.delete('/:contribId', auth, (req, res) => {
         Contrib.findOne(req.params.contribId, function(err, result) {
             if (err)
                 res.status(500).send(err);
@@ -37,33 +45,40 @@ export default new Controller(router => {
         });
     });
 
-	// POST /contrib
-	router.post('', auth, (req, res) => {
+    // POST /contrib
+    router.post('', auth, (req, res) => {
         req.body.userId = req.user._id;
-		Contrib.create(req.body, function(err, result) {
-			if (err) 
-				res.status(500).send(err);
-			else
-				res.status(200).send(result);
-		});
-	});
+        Contrib.create(req.body, function(err, result) {
+            if (err)
+                res.status(500).send(err);
+            else
+                res.status(200).send(result);
+        });
+    });
 
-	// GET /contrib/?feedId=<id>
-	router.get('/', (req, res) => {
-		let query = {};
-		if (req.query.feedId) 
-			query.feed = req.query.feedId;
+    // GET /contrib/?feedId=<id>
+    router.get('/', (req, res) => {
+        let query = {};
+        if (req.query.feedId)
+            query.feed = req.query.feedId;
         if (req.query.userId)
             query.creator = req.query.userId;
 
-		let dbQuery = Contrib.find(query);
+        let dbQuery = Contrib.find(query);
         dbQuery.populate('creator', 'username');
         dbQuery.populate('feed', 'name');
         dbQuery.exec(function(err, result) {
             if (err)
                 res.status(500).send(err);
             else
-                res.status(200).send(result);
+                User.populate(result, {path: 'feed.owners', select: 'username'}, function(err2, result2) {
+                    if (err2)
+                        res.status(500).send(err2);
+                    else if (!result2)
+                        res.status(404).send();
+                    else
+                        res.status(200).send(result2);
+                });
         });
     });
 
